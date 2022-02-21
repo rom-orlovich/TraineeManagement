c1a0c80a8852ac5a7aedb6acaa222f13ed070bab
vina188 <madman280797@gmail.com>
2022-02-21 10:12:43 +0200

    change folder structure

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -4,10 +4,10 @@ import { Route } from "react-router";
 import App from "../App";
 import Layout from "../layout/Layout";
 import Overview from "../pages/Overview/Overview";
-import Financial from "../pages/Financial";
+import Financial from "../pages/Financial/Financial";
 import Trainees from "../pages/Trainees";
-import Calender from "../pages/Calender";
-import Settings from "../pages/Settings";
+import Calender from "../pages/Calender/Calender";
+import Settings from "../pages/Settings/Settings";
 import Leads from "../pages/Leads/Leads";
 function Router() {
   return (
