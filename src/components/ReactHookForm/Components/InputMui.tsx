f362b47b3ba210a797cc2a3a5b0bcd7ded67d6d7
vina188 <madman280797@gmail.com>
2022-03-21 12:45:17 +0200

    FIX STYLE ERROR6

--- src/components/ReactHookForm/Components/InputMui.tsx
+++ src/components/ReactHookForm/Components/InputMui.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { FieldValues } from "react-hook-form";
+import { classNameMaker } from "../../../helpers/helperFunction";
 
 import { FormComponetsExportMui } from "../../MUI/FormComponetsExport/FormComponetsExportMui";
 import { InputMuiType } from "../TypesFormComponent";
 import { UtilitesRHF } from "../UtilitesRHF";
+import ST from "./InputMui.module.scss";
 const { Controller } = UtilitesRHF;
 
 const { TextField } = FormComponetsExportMui;
@@ -13,6 +15,7 @@ function InputMui<T extends FieldValues>({
   label,
   control,
   defaultValue,
+  className,
   ...rest
 }: InputMuiType<T>) {
   return (
@@ -25,6 +28,7 @@ function InputMui<T extends FieldValues>({
           label={label}
           helperText={error ? error.message : null}
           error={!!error}
+          className={classNameMaker(ST, className)}
           {...field}
           {...rest}
         ></TextField>
