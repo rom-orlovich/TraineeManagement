15c0cb3092474185b56de989e75c26798b89f585
Rom Orlovich <madman280797@gmail.com>
2022-05-09 00:00:52 +0300

    delete some lines

--- src/pages/Activities/Activities.tsx
+++ src/pages/Activities/Activities.tsx
@@ -14,17 +14,9 @@ function Activities({ className }: propsType) {
   const { selectState, SelectInputEL } = useGetManageSelectInputState(
     optionSelect[3]
   );
-  // const [selectState, setSelectState] = useState<"incomes" | "expenses">(
-  //   "incomes"
-  // );
+
   const dataFit = dataProvider.activities;
 
-  // const { rows, columns } =
-  //   selectState === "incomes"
-  //     ? dataFit.table[0]
-  //     : selectState === "expenses"
-  //     ? dataFit.table[1]
-  //     : { rows: [], columns: [] };
   const dataResSelect = {
     incomes: dataFit.table[0],
     expenses: dataFit.table[1],
@@ -36,10 +28,6 @@ function Activities({ className }: propsType) {
     <section className={classNameMaker(ST.activities_layout, className)}>
       <div className={classNameMaker(ST.upper_section)}>
         <div className={classNameMaker(ST.select_input)}>
-          {/* <SelectInput
-            data={optionSelect[3]}
-            SetValueOnChange={setSelectState}
-          ></SelectInput> */}
           <SelectInputEL />
         </div>
 
